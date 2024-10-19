7f35b52feba240ebef7e66f6252d973a69834587
VaibhavNavneet <navneets.7090@gmail.com>
2024-10-19 21:01:19 +0530

    div-2 round-979

---
+++ B_Minimise_Oneness.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class B_Minimise_Oneness {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int t=in.nextInt();
+        while(t-->0)
+        {
+            int n=in.nextInt();
+            
+            String s="";
+           for(int i=0;i<n-1;i++)
+           System.out.print("0");
+           System.out.print("1");
+           System.out.println();
+        }
+        }
+    }
